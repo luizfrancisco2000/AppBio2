d1ac78a2dcc40705229cee4bd001930e82a26a3e
Luiz Francisco <luizfrancisco2000@gmail.com>
2018-10-08 14:16:12 -0400

    Inicio API do app de biologia

---
+++ src/main/java/com/fabrica/api/Biologia/BiologiaApplication.java
@@ -0,0 +1,12 @@
+package com.fabrica.api.Biologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiologiaApplication.class, args);
+	}
+}
